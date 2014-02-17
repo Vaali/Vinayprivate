70f4930ebe8c248f0918f6c692e38284a34bd923
vaali <pamarthi.vinay@gmail.com>
2014-02-16 23:24:02 -0500

    Added youtubelist for myalbum

--- databasepushnav1_album.py
+++ databasepushnav1_album.py
@@ -2,6 +2,8 @@ from multiprocessing import Process
 import os
 import re
 import time
+import logging
+logging.basicConfig(filename='example.log',level=logging.DEBUG,format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
 def func(strg):
 	os.system(strg)
 
@@ -14,7 +16,7 @@ t1=time.time()
 
 for dirs in os.listdir(directory):
   	found = re.search(r'[0-9]+',str(dirs),0)
-  	print dirs
+  	logging.warning(dirs)
   	#print f
 #ipdir = str(sys.argv[1])
   	if found:
@@ -38,7 +40,7 @@ for dirs in os.listdir(directory):
 		#foldlist.append(strg)
 
 n = len(foldlist)
-print n
+logging.warning(n)
 count=0
 #m = 1
 k = n/m
@@ -54,17 +56,17 @@ for j in range(1,blocknumber+1):
 	else:
 		q = m
 	for i in foldlist[p*m:p*m+q]:
-		print i
+		logging.warning(i)
 		p = Process(target=func,args=(i,))
 		findex = i.rfind(' ')
 		foldname = i[findex+1:len(i)]
-		print "folder name =========================="
-		print foldname
+		logging.warning("folder name ==========================")
+		logging.warning(foldname)
 		count = count + 1
-		print "count="+str(count)
+		logging.warning("count="+str(count))
 		jobs.append(p)
 		p.start()
 	p.join()
-print time.time()-t1
+logging.warning(time.time()-t1)
 
 		
