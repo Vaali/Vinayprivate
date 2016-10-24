c910bf0f68d5a9e8fc98998718bd0a11987532fb
Vaali <pamarthi.vinay@gmail.com>
2016-10-23 22:31:49 -0400

    changed log directory

--- combineartistmatrix.py
+++ combineartistmatrix.py
@@ -10,21 +10,20 @@ from multiprocessing import Pool
 import logging
 import ConfigParser
 import time
-import logging.handlers
-config = ConfigParser.ConfigParser()
+import loggingmodule
 reload(sys)
-if(not os.path.exists('logdir')):
-	os.mkdir('logdir')
+if(not os.path.exists('logs')):
+	os.mkdir('logs')
 else:
     try:
-        os.remove('logdir/combined.txt')
+        os.remove('logs/combined.txt')
     except OSError:
         pass
 sys.setdefaultencoding('utf8')
-formatter = logging.Formatter('%(message)s')
+'''formatter = logging.Formatter('%(message)s')
 logger_genre = logging.getLogger('simple_logger')
 hdlr_1 = logging.handlers.RotatingFileHandler(
-              'logdir/combined.txt', maxBytes=100*1024*1024*1024, backupCount=500)
+              'logs/combined.txt', maxBytes=100*1024*1024*1024, backupCount=500)
 hdlr_1.setFormatter(formatter)
 logger_genre.addHandler(hdlr_1)
 logger_genre = logging.getLogger('simple_logger')
@@ -35,25 +34,25 @@ hdlr_2 = logging.handlers.RotatingFileHandler(
               'combined_artists.log', maxBytes=1024*1024*1024, backupCount=10)
 hdlr_2.setFormatter(formatter1)
 logger_errors.addHandler(hdlr_2)
-logger_errors = logging.getLogger('simple_logger1')
+logger_errors = logging.getLogger('simple_logger1')'''
 
 def combinefiles(directory):
     try:
         d = directory
         if(len(d.strip()) == 0):
-			return
+            return
         directory = d.strip()
         path = directory +'/matrix.txt'
         print path
         if os.path.exists(path):
             fopen = codecs.open(path,'r','utf-8')
             for line in fopen: 
-                    logger_genre.error(line)
-                    #print line
+                logger_genre.error(line)
+                #print line
         else:
-			print 'nofile'
+            print 'nofile'
     except Exception as e:
-        logger_decisions.exception(e)
+        logger_errors.exception(e)
 
 
 directory = raw_input("Enter directory: ")
@@ -62,6 +61,9 @@ m=int(m)
 foldlist = list()
 jobs=[]
 t1=datetime.now()
+logger_genre = loggingmodule.initialize_logger('combined.txt',False)
+logger_errors = loggingmodule.initialize_logger1('combined_artisterrors.log')
+
 for dirs in os.listdir(directory):
   	found = re.search(r'[0-9]+',str(dirs),0)
   	if found:
@@ -72,13 +74,14 @@ for dirs in os.listdir(directory):
 					continue
 				strg = os.path.join(curr_dir,sd)
 				foldlist.append(strg)
+print len(foldlist)
 try:
 	p =Pool(processes=int(m))
 	p.map(combinefiles,foldlist)
 	p.close()
 	p.join()
 except Exception as e:
-	logger_decisions.exception(e)
+	logger_errors.exception(e)
 t2=datetime.now()
 
 print "time=" +str(t2-t1)
