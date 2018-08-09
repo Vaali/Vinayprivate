be243c3fdaf058f6246a5fc00e89b4917e128d53
Vaali <pamarthi.vinay@gmail.com>
2018-08-09 00:29:04 -0400

    logger cahnges
    logger changes

--- loggingmodule.py
+++ loggingmodule.py
@@ -1,10 +1,13 @@
 import logging
 import logging.handlers
 import sys
+import os
 
 
 reload(sys)
 sys.setdefaultencoding('utf8')
+if(not os.path.exists('logs')):
+    os.mkdir('logs',0755)
 def initialize_logger(logfileName,time= True):
     if(time == True):
         formatter = logging.Formatter('%(asctime)s %(message)s')
@@ -12,7 +15,7 @@ def initialize_logger(logfileName,time= True):
         formatter = logging.Formatter('%(message)s')
     currlogger = logging.getLogger('simple_logger')
     hdlr = logging.handlers.RotatingFileHandler(
-            logfileName, maxBytes=10240*1024*1024, backupCount=500)
+            'logs/'+logfileName, maxBytes=10240*1024*1024, backupCount=500)
     hdlr.setFormatter(formatter)
     currlogger.addHandler(hdlr)
     return currlogger
@@ -21,8 +24,8 @@ def initialize_logger1(logfilename):
     formatter = logging.Formatter('%(asctime)s %(message)s')
     currlogger = logging.getLogger('simple_logger_2')
     hdlr = logging.handlers.RotatingFileHandler(
-            logfilename, maxBytes=10240*1024*1024, backupCount=500)
+            'logs/'+logfilename, maxBytes=10240*1024*1024, backupCount=500)
     hdlr.setFormatter(formatter)
     currlogger.addHandler(hdlr)
-
+    return currlogger
 
