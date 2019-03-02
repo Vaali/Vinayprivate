2f30a56a6a65d90cd948c1fc96e844d22a515e2a
Vaali <pamarthi.vinay@gmail.com>
2019-03-01 19:53:50 -0500

    new functions
    Adding new fuctions

--- songsutils.py
+++ songsutils.py
@@ -1,9 +1,28 @@
 import sys
 import codecs
 import os
+import shutil
 
 reload(sys)
 sys.setdefaultencoding('utf8')
 
 def is_songname_same_artistname(songname,artistname):
     return (songname.lower() == artistname.lower())
+
+def get_artistid(directory):
+    directory = directory.strip()
+    dindex = directory.rfind("/")
+    artistId = directory[dindex+1:]
+    return artistId
+
+
+def moveFiles(filename,movetype):
+    fname = filename[filename.rfind('/')+1:]
+    foldername = filename[:filename.rfind('/')]
+    output_directory = foldername+'/'+movetype
+    if(not os.path.exists(output_directory)):
+		os.makedirs(output_directory)
+    if(os.path.exists(os.path.join(output_directory, fname))):
+		os.remove(os.path.join(output_directory, fname))
+    dest = output_directory+'/'+fname
+    shutil.move(filename,dest)
