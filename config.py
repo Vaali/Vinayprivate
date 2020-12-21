ad2a29c8ed3746b23dbb31612f670f702f8375d8
Vaali <pamarthi.vinay@gmail.com>
2020-12-21 00:21:26 -0500

    config organising
    organising config file into sections and adding solr changes to config

--- config.py
+++ config.py
@@ -1,31 +1,55 @@
 #number of proceses
-NumberOfProcesses = 10
-NumberofThreads = 4
+NumberOfProcesses = 20
+NumberofThreads = 20
+
+#incremental/full
+IsIncremental = 0
+
+IsCrawlingYoutube = 1
+
+#crawling freshness
+SkipRecentlyCrawledDirectories = 0
+CrawlDaysWindow = 19
+
+
+#step 1: get uniques songs list from discogs data
+#getsongslist.py
+#step2: crawl youtube to get videos
+#getvideosfinal.py
+#step 3: generate xmls from the dump file.
+#generatexmls_discogs.py
+DiscogsDataDirectory = 'smalldata'
+NumberofFolders = 2
+IsSoundCloud = 0
+# 1 for using youtubeapi 0 for using youtube dl
+IsYoutudeApi = 0
+CacheDir = '/Volumes/Secondone/sourcefiles/solr_newData11_old/newdir/cache'
+
+TopSongs = 10 # used to calculat averages of counts etc
+
 
 #updatexml_new.py
 # 1 for updating viewcounts , 0 for updating genre tags
 IsUpdateViewCounts = 1
 #directory where xmls are present
-DataDirectory = 'solr_newData112'
-# 1 for using youtubeapi 0 for using youtube dl
-IsSoundCloud = 0
-IsYoutudeApi = 0
-CacheDir = '/Volumes/Secondone/sourcefiles/solr_newData11_old/newdir/cache'
+DataDirectory = 'solr_newData1222/deletedvideos'
+
 
 #recrawling
 #RecrawlDirectory = '/Volumes/Secondone/sourcefiles/solr_newData11_old/deletedvideos'
 #RecrawlOutputDirectory = '/Volumes/Secondone/sourcefiles/solr_newData11_old/newdir/tt'
-RecrawlDirectory = 'test1'
-RecrawlOutputDirectory = 'test1/xxxx'
-#datacrawling
-#DiscogsDataDirectory = 'test_discogs1'
-DiscogsDataDirectory = 'fhiphop1'
-NumberofFolders = 2
-IsCrawlingYoutube = 0
-SkipRecentlyCrawledDirectories = 0
-CrawlDaysWindow = 45
-IsIncremental = 0
-TopSongs = 10 # used to calculat averages of counts etc
+RecrawlDirectory = 'solr_newData1122'
+RecrawlOutputDirectory = 'solr_newData1122/xxxx'
+
+#solr url connections
+SolrDataUrl = 'http://aurora.cs.rutgers.edu:8181/solr/discogs_data_test'
+SolrDiscogsArtistsUrl = 'http://aurora.cs.rutgers.edu:8181/solr/discogs_artists'
+SolrArtistsUrl = 
+SolrGenresUrl = 'http://aurora.cs.rutgers.edu:8181/solr/similar_genres'
+SolrGenreTagsUrl = 'http://aurora.cs.rutgers.edu:8181/solr/genretags'
+SolrSimilarArtistsUrl = 'http://aurora.cs.rutgers.edu:8181/solr/similar_artists1'
+
+
 
 
 
